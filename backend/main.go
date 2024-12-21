f37d5d588f2c8867eaad6da07d839cd9e27b4cee
Hossein Nasiri <hossein.nasiri.sovari@gmail.com>
2024-12-21 22:58:55 +0330

    feat: basic router

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	_ "context"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/hossein-nas/analytics_aggregator/router"
 )
 
 func main() {
-	fmt.Println("Hey!")
+	r := router.Router()
+	fmt.Println("Starting server on the port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
